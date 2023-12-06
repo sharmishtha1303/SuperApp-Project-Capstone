628e4313b78d5f9513e945cf4e827e346c4ea2f2
Sharmishtha Sapkal <sharusapkal16344@gmail.com>
2023-12-06 19:01:52 +0530

    error solved

--- src/Pages/RegistrationPage.jsx
+++ src/Pages/RegistrationPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Picture from "../Components/Registration/Picture";
-import Form from "../Components/Registration/form";
+import Form from "../Components/Registration/Form";
 const RegistrationPage = () => {
   return (
     <>
